5c3a0288214bad998944339d73e97b42feedc8c4
nastasiia <anasteishapoliakova@icloud.com>
2021-05-16 13:57:18 +0300

    Passed tasks for variables.;

---
+++ variables.js
@@ -0,0 +1,26 @@
+// Task 1.
+
+let admin;
+
+let name;
+
+name = 'Джон';
+
+admin = name;
+
+console.log(admin);
+
+// Task 2.
+
+let myPlanetName = 'Земля';
+
+let currentUserName = 'Джон';
+
+console.log(myPlanetName, currentUserName);
+
+// Task 3.
+
+// const BIRTHDAY = '18.04.1982'; использовать заглавные буквы? - Да. (значение неизменно)
+
+// const AGE = someCode(BIRTHDAY); а здесь? - Нет. (значение может изменяться)
+
