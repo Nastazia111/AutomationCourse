c3d655c0d320d0d7ae20e150190a022a6504df2e
nastasiia <anasteishapoliakova@icloud.com>
2021-06-04 12:53:38 +0300

    passed block conversions;

---
+++ conversions.js
@@ -0,0 +1,41 @@
+// String conversion
+let value = true;
+
+console.log(typeof value);
+
+// Numerical conversion
+value = String(value);
+
+console.log(typeof value);
+
+//
+console.log("6" / "2");
+
+//
+let str = "12345";
+
+console.log(typeof str);
+
+let num = Number(str);
+
+console.log(typeof num);
+
+//
+let age = Number ("Some text");
+
+console.log(age);
+
+//
+console.log(Number("123"));
+console.log(Number("some text"));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(undefined));
+
+// Boolean transformation
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean("Text"));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
